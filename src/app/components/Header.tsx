56daa2f5bf47658675d0149d4f9059f39a99ebe3
tillrostalski <rostis@hotmail.de>
2023-08-20 12:16:41 +0200

    changes hsn bg color

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import { RiAncientGateFill } from "react-icons/ri";
 const Header = () => {
   return (
     <header>
-      <nav className="flex flex-row fixed left-0 top-0 w-full h-10 bg-hns-blue items-center text-white font-light px-4 z-10">
+      <nav className="flex flex-row fixed left-0 top-0 w-full h-10 bg-primary-button items-center text-white font-light px-4 z-10">
         <h1>Vhipple</h1>
         <span className="flex-grow"></span>
         <div className="flex flex-row gap-4">

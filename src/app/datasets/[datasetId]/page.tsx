253d29e2f755c17306302aff20b8e3bc9f185a77
tillrostalski <rostis@hotmail.de>
2023-09-01 22:04:28 +0200

    immoc

--- src/app/datasets/[datasetId]/page.tsx
+++ src/app/datasets/[datasetId]/page.tsx
@@ -1,49 +1,49 @@
 "use client";
-import React, { useMemo, useState } from "react";
 import MainWrapper from "@/app/components/MainWrapper";
-import { useLiveQuery } from "dexie-react-hooks";
-import { getDataset } from "@/app/db/utils";
-import DatasetHeader from "./components/DatasetHeader";
-import CohortList from "./components/CohortList";
-import { DatasetInfo } from "./components/DatasetInfo";
-import DisplayTabs from "./patients/components/DisplayTabs";
-import ResourcesTableContainer from "./components/ResourcesTableContainer";
-import { availableDatasetDisplayTabs } from "./lib/constants";
+import SearchBar from "@/app/components/SearchBar";
+import React, { useState } from "react";
 
-const page = ({ params }: { params: { datasetId: string } }) => {
-  const { datasetId } = params;
-  const [displayTab, setDisplayTab] = useState<
-    (typeof availableDatasetDisplayTabs)[number]
-  >(availableDatasetDisplayTabs[0]);
+const page = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const dataset = useLiveQuery(() => getDataset(datasetId));
-
-  if (!dataset) {
-    return null;
-  }
-
-  const contentToRender = {
-    Overview: <DatasetInfo dataset={dataset} />,
-    Cohorts: <CohortList dataset={dataset} />,
-    Resources: <ResourcesTableContainer dataset={dataset} />,
-  };
+  const handleSearch = () => {};
 
   return (
     <MainWrapper>
-      <div className="flex flex-col w-full h-full gap-4">
-        <DatasetHeader dataset={dataset} />
-        {!dataset.description ? (
-          <span className="text-gray-500">No description.</span>
-        ) : (
-          <span className="text-gray-500">{dataset.description}</span>
-        )}
-        <DisplayTabs
-          availableDisplayTabs={availableDatasetDisplayTabs}
-          displayTab={displayTab}
-          setDisplayTab={setDisplayTab}
-        />
-        {displayTab in contentToRender &&
-          contentToRender[displayTab as keyof typeof contentToRender]}
+      <div className="flex flex-col">
+        <div>
+          <SearchBar
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleSearch={handleSearch}
+            placeholder="Search"
+          />
+        </div>
+        <div>
+          <div className="grid grid-cols-4 gap-6">
+            <div className="flex flex-col items-center justify-center bg-white font-bold rounded-md shadow-md py-4 transition hover:shadow-xl hover:cursor-pointer group">
+              <span className="text-2xl text-gray-500 group-hover:text-primary-button-hover">
+                Patients
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center bg-white font-bold rounded-md shadow-md py-4 transition hover:shadow-xl hover:cursor-pointer group">
+              <span className="text-2xl text-gray-500 group-hover:text-primary-button-hover">
+                Resources
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center bg-white font-bold rounded-md shadow-md py-4 transition hover:shadow-xl hover:cursor-pointer group">
+              <span className="text-2xl text-gray-500 group-hover:text-primary-button-hover">
+                Charts
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center bg-white font-bold rounded-md shadow-md py-4 transition hover:shadow-xl hover:cursor-pointer group">
+              <span className="text-2xl text-gray-500 group-hover:text-primary-button-hover">
+                {" "}
+                Cohorts
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </MainWrapper>
   );
